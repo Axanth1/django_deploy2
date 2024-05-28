62418b34a49b8f0cb19950de3a8b5002f67a034f
Alexandros Xanthopoulos <xanthopoulosa22@gmail.com>
2024-05-28 12:49:57 +0300

    gfdgf

--- run.sh
+++ run.sh
@@ -2,5 +2,8 @@
 set -e
 echo "Collecting static assets..."
 python manage.py collectstatic --noinput
+echo "Migrating the database..."
+python manage.py migrate
 echo "Starting the application..."
+
 gunicorn app.wsgi
